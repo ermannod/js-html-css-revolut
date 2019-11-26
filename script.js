0c261e08551d9418733235a9cf88dc9c5a7035eb
ermannod <erma-d@hotmail.com>
2019-11-26 18:10:42 +0100

    added mouse hover version

--- script.js
+++ script.js
@@ -8,3 +8,16 @@ $(document).ready(function() {
       }
   });
 });
+
+
+// with mouse hover
+// $(document).ready(function() {
+//     $(".listitem").mouseenter(function(){
+//       if ($(this).children(".dropdown").is(":visible")) {
+//         $(".dropdown").slideUp();
+//       } else {
+//         $(".dropdown").slideUp();
+//         $(this).children(".dropdown").slideDown();
+//       }
+//   });
+// });
